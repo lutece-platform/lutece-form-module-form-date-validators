b2365276b67e8f39d34b1d6edb43b3321710363c
Jon Harper <jon.harper87@gmail.com>
2015-12-18 14:25:42 +0000

    FORMDATEVALIDATORS: fix comment for MySQL
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/directory-form-workflow/module-form-date-validators@79522 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/form/modules/datevalidators/plugin/init_db_form_date_validators.sql
+++ src/sql/plugins/form/modules/datevalidators/plugin/init_db_form_date_validators.sql
@@ -7,8 +7,8 @@ INSERT INTO form_date_validators_comparator(id_comparator,name,class) VALUES(6, 
 
 INSERT INTO form_date_validators_operator(id_operator,name,class) VALUES(1, '+', 'fr.paris.lutece.plugins.form.modules.datevalidators.business.operator.OperatorAdd');
 INSERT INTO form_date_validators_operator(id_operator,name,class) VALUES(2, '-', 'fr.paris.lutece.plugins.form.modules.datevalidators.business.operator.OperatorSubstract');
---INSERT INTO form_date_validators_operator(id_operator,name,class) VALUES(3, '*', 'fr.paris.lutece.plugins.form.modules.datevalidators.business.operator.OperatorMultiply');
---INSERT INTO form_date_validators_operator(id_operator,name,class) VALUES(4, '/', 'fr.paris.lutece.plugins.form.modules.datevalidators.business.operator.OperatorDivide');
+-- INSERT INTO form_date_validators_operator(id_operator,name,class) VALUES(3, '*', 'fr.paris.lutece.plugins.form.modules.datevalidators.business.operator.OperatorMultiply');
+-- INSERT INTO form_date_validators_operator(id_operator,name,class) VALUES(4, '/', 'fr.paris.lutece.plugins.form.modules.datevalidators.business.operator.OperatorDivide');
 
 INSERT INTO form_date_validators_unit(id_unit,name,class) VALUES(1, 'module.form.datevalidators.unit.label.year', 'fr.paris.lutece.plugins.form.modules.datevalidators.business.unit.UnitYear');
 INSERT INTO form_date_validators_unit(id_unit,name,class) VALUES(2, 'module.form.datevalidators.unit.label.month', 'fr.paris.lutece.plugins.form.modules.datevalidators.business.unit.UnitMonth');
